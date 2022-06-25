1dad426e8c1218b90c6eb55b354a3af2f00d8966
Alejius <alejoperna@gmail.com>
2022-06-25 18:22:56 -0300

    AirApp1

---
+++ app/src/main/java/com/example/airappv2/HomeActivity.kt
@@ -0,0 +1,22 @@
+package com.example.airappv2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+        temperatura.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
